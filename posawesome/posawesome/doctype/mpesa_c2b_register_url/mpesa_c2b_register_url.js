14e9a616732842ba286dcd0b4c76e4e2501decf2
muhammadqasimc <42121445+muhammadqasimc@users.noreply.github.com>
2022-09-21 20:00:41 +0200

    Delete posawesome/posawesome/doctype/mpesa_c2b_register_url directory

--- posawesome/posawesome/doctype/mpesa_c2b_register_url/mpesa_c2b_register_url.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, Youssef Restom and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Mpesa C2B Register URL', {
-	// refresh: function(frm) {
-
-	// }
-});
